61712dabc49823430493cbc18ca7561e0dbbf599
M Rangga Hendarmin <rangga.hendarmin@pactindo.com>
2025-07-03 11:35:37 +0700

    change for test on jenkins for GIT

---
+++ Test Suites/TestSuite-1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite-1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9bdace52-dc51-4afe-add1-d8cf499dd0cd</testSuiteGuid>
+   <testCaseLink>
+      <guid>3fc5edb1-9c12-49d8-88d5-36b41e27c5ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CICD_Tutor</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
